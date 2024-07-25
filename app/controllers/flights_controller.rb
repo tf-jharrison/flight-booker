98180bac12c198da85f03e6de7f6bfc98ee69f1f
John Harrison <jharrison@tracefirst.com>
2024-07-25 16:14:55 +0100

    drop down menus complete

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
 	def index
+		@arrival_airports = Flight.joins(:arrival_airport).pluck("airports.code")
+		@departure_airports = Flight.joins(:departure_airport).pluck("airports.code")
+		@dates = Flight.all.pluck("start")
 	end
 end

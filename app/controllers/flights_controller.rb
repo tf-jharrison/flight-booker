d7be1b6987577f150bdf4c078c6dfe921e07bd27
John Harrison <jharrison@tracefirst.com>
2024-07-26 17:00:44 +0100

    exercise 2 complete

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,12 +3,17 @@ class FlightsController < ApplicationController
 		@arrival_airports = Flight.joins(:arrival_airport).pluck("airports.code")
 		@departure_airports = Flight.joins(:departure_airport).pluck("airports.code")
 		@dates = Flight.all.pluck("start")
-		
-		requested_date = params[:date].split('/').reverse.join('-')
-		requested_arr_airport_id = Airport.find_by(code: params[:arrival_airport]) 
-		requested_dep_airport_id = Airport.find_by(code: params[:departure_airport]) 
-		@available_flights = Flight.joins(:departure_airport, :arrival_airport).
-								where(departure_airport_id: requested_dep_airport_id.id, arrival_airport_id: requested_arr_airport_id.id).
-								where("DATE(start) = ?", requested_date)
+	
+		if params[:commit]
+
+			@number_of_passengers = params[:passenger_number].to_i
+		 	requested_date = params[:date].split('/').reverse.join('-')
+		 	requested_arr_airport_id = Airport.find_by(code: params[:arrival_airport]) 
+		 	requested_dep_airport_id = Airport.find_by(code: params[:departure_airport]) 
+
+			@available_flights = Flight.joins(:departure_airport, :arrival_airport).
+		 							where(departure_airport_id: requested_dep_airport_id.id, arrival_airport_id: requested_arr_airport_id.id).
+		 							where("DATE(start) = ?", requested_date)
+		 end
 	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def time_conversion(minutes)
+		hours = minutes / 60
+		rest = minutes % 60
+		return "#{hours} Hr #{rest} Min"
+	end
 end
